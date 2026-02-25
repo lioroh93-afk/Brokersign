10741037d4fd2377407a6b6c3608f62c6681c431
lioroh93-afk <lioroh93@gmail.com>
2026-02-25 05:09:35 +0200

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     const response = await fetch('https://api.resend.com/emails', {
       method: 'POST',
       headers: {
-        'Authorization': 'Bearer re_R2CkEyey_HhKKp7wcAb99X5C5F7JXgXoU',
+        'Authorization': 'Bearer ' + process.env.RESEND_API_KEY,
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
